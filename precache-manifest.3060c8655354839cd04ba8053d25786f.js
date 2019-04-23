3ff476841e7d9dba21118e32a6ed0405d509750a
annearbor <betting.anne@gmail.com>
2019-04-23 21:06:16 +0200

    Updates

---
+++ precache-manifest.3060c8655354839cd04ba8053d25786f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa4ed7e6f7f7b90d6d9061afce9cf0b9",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "ab505188b6cdc7130747",
+    "url": "/myapp/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "f902a2abba1fde9cb3a5",
+    "url": "/myapp/static/css/main.9c1ea4e4.chunk.css"
+  },
+  {
+    "revision": "ab505188b6cdc7130747",
+    "url": "/myapp/static/js/2.7419dff6.chunk.js"
+  },
+  {
+    "revision": "f902a2abba1fde9cb3a5",
+    "url": "/myapp/static/js/main.40908908.chunk.js"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  },
+  {
+    "revision": "221d2fcf5b815a87e27a95b26f29af3d",
+    "url": "/myapp/static/media/hero-verduu.221d2fcf.jpg"
+  },
+  {
+    "revision": "bd8ee4658dcc1c7c7e31d84479944af0",
+    "url": "/myapp/static/media/verduu_logo.bd8ee465.png"
+  }
+]);
